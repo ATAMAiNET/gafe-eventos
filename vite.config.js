9167427a6b1ce884771d713e0dffc49b92bd529e
Atamainet <atamai2013@gmail.com>
2025-05-31 11:14:33 -0300

    Yo comit!

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mdx from '@mdx-js/rollup'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    { enforce: 'pre', ...mdx() },
+    react({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),
+    react({ jsxImportSource: '@emotion/react' }),
+    react({ jsxRuntime: 'classic' }),
+    react({
+      babel: {
+        presets: ["Fast Refresh"],
+        // Your plugins run before any built-in transform (eg: Fast Refresh)
+        plugins: [["@emotion/babel-plugin", { sourceMap: true }]],
+        // Use .babelrc files
+        babelrc: true,
+        // Use babel.config.js files
+        configFile: true,
+      }
+    }),
+    react({
+      babel: {
+        parserOpts: {
+          plugins: ['decorators-legacy'],
+        },
+      },
+    })
+  ],
 })
